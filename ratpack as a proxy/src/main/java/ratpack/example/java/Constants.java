4632964d82b3408bfa06eb0ac87c49787e812895
Madireddy <AMadireddy@ad.harman.com>
2017-10-26 14:31:49 -0500

    Smart App

---
+++ ratpack as a proxy/src/main/java/ratpack/example/java/Constants.java
@@ -0,0 +1,15 @@
+package ratpack.example.java;
+
+public class Constants {
+
+	public static String smartAppEndpointURL = "https://graph.api.smartthings.com/api/smartapps/installations/aa2c3ee6-39f0-4b70-b96e-dd920a8df1a9/";
+
+	public static String setLevlPath = "setlevel/";
+
+	public static String getSwitchesPath = "switches";
+
+	public static String authorizationKey = "Bearer 28833c3c-daae-4708-8352-dafff33c59cb";
+
+
+
+}
